4aebb210d24dc7e1618d8451052365ed0365dab6
Fabricio Bedin <fabricioobedin@gmail.com>
2020-03-07 12:38:25 -0300

    Intent examples added

--- app/src/main/java/io/github/fabriciobedin/androidexamples/MainActivity.java
+++ app/src/main/java/io/github/fabriciobedin/androidexamples/MainActivity.java
@@ -2,13 +2,23 @@ package io.github.fabriciobedin.androidexamples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void intents(View view) {
+        Intent intent = new Intent(this, IntentActivity.class);
+        startActivity(intent);
     }
 }
